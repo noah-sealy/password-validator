a64eb457f0fb397be30959a408f5366560b6e546
noah-sealy <noahsealy@hotmail.com>
2018-06-07 17:25:16 -0300

    Java Validator class v1 created

--- app/src/main/java/com/example/noahsealy/assignment2/Validator.java
+++ app/src/main/java/com/example/noahsealy/assignment2/Validator.java
@@ -1,5 +1,15 @@
 package com.example.noahsealy.assignment2;
 
 public class Validator {
-
+    public int check (String s) {
+        int size = s.length();
+        String badPass = "password";
+        if (size < 8) {
+            return 0;
+        } else {
+            if (!s.equalsIgnoreCase(badPass))
+                return 1;
+        }
+        return 0;
+    }
 }
